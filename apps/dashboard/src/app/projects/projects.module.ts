1a01af5f649ccb0a69ecb795ebd9fd830fb5355e
aakatev <aakatev@mail.ccsf.edu>
2019-02-12 09:41:57 -0800

    Finished course

--- apps/dashboard/src/app/projects/projects.module.ts
+++ apps/dashboard/src/app/projects/projects.module.ts
@@ -4,9 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '@workshop/material';
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsComponent } from './projects.component';
+import { ProjectsListComponent } from './projects-list/projects-list.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
 
 @NgModule({
-  declarations: [ProjectsComponent],
+  declarations: [ProjectsComponent, ProjectsListComponent, ProjectDetailsComponent],
   imports: [
     CommonModule,
     ProjectsRoutingModule,
